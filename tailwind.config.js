e0e329d80cff03d1e7dbb96b1a248241604f8c39
mrmohsensami <mrmohsensami@gmail.com>
2022-06-19 23:56:27 +0430

    initial tailwind instagram and header

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      screens: {
+        sm: '600px',
+        md: '728px',
+        lg: '984px',
+        xl: '1000px',
+        '2xl': '1240px',
+      },
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+      center: true
+    },
+    extend: {},
+  },
+  plugins: [],
+}
